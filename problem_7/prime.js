6170379c83bd6e58cdae6efd7cd6b9b043cf263a
Domhnall <domhnallmurphy@hotmail.com>
2016-01-30 12:14:41 +0000

    Negative numbers cannot be prime ... discuss.

--- problem_7/prime.js
+++ problem_7/prime.js
@@ -2,6 +2,7 @@ var PRIME = (function(p) {
   "use strict";
 
   p.is_prime = function(val) {
+    if(val<0){ return false; }
     let half = Math.floor(val/2)+1;
     for(let i = 2; i<half; i++) {
       if(val%i===0){
